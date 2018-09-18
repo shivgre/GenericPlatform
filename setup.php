fe68509dc86a018875813fd3cdc9e00080f9ddae
Shiv Kumar <shivkumarsuccessful@gmail.com>
2018-09-18 18:28:18 +0530

    Deleted old code.

--- setup.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!isset($_SESSION["dbHost"])) {
-    $_SESSION["dbHost"] = "localhost";
-    $_SESSION["dbUsername"] = "genericnew";
-    $_SESSION["dbPassword"] = "Capstone2018!";
-    $_SESSION["dbName"] = "genericnew";
-    $_SESSION['baseURL'] = "http://home.localhost/GenericNew/GenericPlatform/main.php?";
-}
